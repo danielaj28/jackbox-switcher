31b97b69783f12ef39c1c62729c6ec5c3e1ed5bb
Daniel Ward <danielaj_28@hotmail.co.uk>
2021-03-01 01:22:02 +0000

    Multiple steam paths now possible in config

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.Windows.Forms;
 
@@ -7,6 +9,7 @@ namespace Jackbox_Switcher
 	static class Program
 	{
 		public static String PreviousProcess { get; private set; }
+		public static List<String> steamLocations = new List<String>();
 
 		/// <summary>
 		/// The main entry point for the application.
@@ -21,6 +24,11 @@ namespace Jackbox_Switcher
 
 		internal static void ChangePack(int pack)
 		{
+			if (steamLocations.Count == 0)
+			{
+				steamLocations.AddRange(ConfigurationManager.AppSettings["steamLocations"].Split(','));
+			}
+
 			if (Program.PreviousProcess != null)
 			{
 				try
@@ -32,33 +40,59 @@ namespace Jackbox_Switcher
 				}
 			}
 
-			Process jbProcess = new Process();
+			String PackPath = "";
+
 			switch (pack)
 			{
 				case 1:
-					jbProcess.StartInfo.FileName = @"D:\Steam\steamapps\common\The Jackbox Party Pack\The Jackbox Party Pack.exe";
+					PackPath = @"The Jackbox Party Pack\The Jackbox Party Pack.exe";
 					break;
 				case 2:
-					jbProcess.StartInfo.FileName = @"D:\Steam\steamapps\common\The Jackbox Party Pack 2\The Jackbox Party Pack 2.exe";
+					PackPath = @"The Jackbox Party Pack 2\The Jackbox Party Pack 2.exe";
 					break;
 				case 3:
-					jbProcess.StartInfo.FileName = @"D:\Steam\steamapps\common\The Jackbox Party Pack 3\The Jackbox Party Pack 3.exe";
+					PackPath = @"The Jackbox Party Pack 3\The Jackbox Party Pack 3.exe";
 					break;
 				case 4:
-					jbProcess.StartInfo.FileName = @"D:\Steam\steamapps\common\The Jackbox Party Pack 4\The Jackbox Party Pack 4.exe";
+					PackPath = @"The Jackbox Party Pack 4\The Jackbox Party Pack 4.exe";
 					break;
 				case 5:
-					jbProcess.StartInfo.FileName = @"D:\Steam\steamapps\common\The Jackbox Party Pack 5\The Jackbox Party Pack 5.exe";
+					PackPath = @"The Jackbox Party Pack 5\The Jackbox Party Pack 5.exe";
 					break;
 				case 6:
-					jbProcess.StartInfo.FileName = @"D:\Steam\steamapps\common\The Jackbox Party Pack 6\The Jackbox Party Pack 6.exe";
+					PackPath = @"The Jackbox Party Pack 6\The Jackbox Party Pack 6.exe";
 					break;
 				case 7:
-					jbProcess.StartInfo.FileName = @"D:\Steam\steamapps\common\The Jackbox Party Pack 7\The Jackbox Party Pack 7.exe";
+					PackPath = @"The Jackbox Party Pack 7\The Jackbox Party Pack 7.exe";
 					break;
 			}
-			jbProcess.Start();
-			Program.PreviousProcess = jbProcess.ProcessName;
+
+			Process jbProcess = new Process();
+
+			foreach (var steamLocation in steamLocations)
+			{
+				try
+				{
+					jbProcess.StartInfo.FileName = steamLocation + PackPath;
+					jbProcess.Start();
+					break;
+				}
+				catch (Exception)
+				{
+				}
+			}
+
+			try
+			{
+				Program.PreviousProcess = jbProcess.ProcessName;
+			}
+			catch (Exception)
+			{
+				if (MessageBox.Show("Could not find Jackbox Pack, would you like to add a new steam library location?", "Pack Not Found", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+				{
+					//TODO: Use Folder browser dialog to find location and add it to library list
+				}
+			}
 		}
 	}
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Jackbox Switcher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Switch between Jackbox party packs with ease")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("danieljaward.com")]
 [assembly: AssemblyProduct("Jackbox Switcher")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
