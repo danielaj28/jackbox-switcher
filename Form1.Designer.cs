8eccce7953629be5921ddde2ceba5067387e0584
Daniel Ward <danielaj_28@hotmail.co.uk>
2021-03-08 23:52:39 +0000

    Updated UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -47,88 +47,113 @@ namespace Jackbox_Switcher
 			// 
 			// btn1
 			// 
+			this.btn1.BackColor = System.Drawing.SystemColors.MenuHighlight;
 			this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btn1.Location = new System.Drawing.Point(12, 27);
+			this.btn1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btn1.Location = new System.Drawing.Point(18, 58);
+			this.btn1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.btn1.Name = "btn1";
-			this.btn1.Size = new System.Drawing.Size(66, 46);
+			this.btn1.Size = new System.Drawing.Size(99, 71);
 			this.btn1.TabIndex = 1;
 			this.btn1.Text = "1";
-			this.btn1.UseVisualStyleBackColor = true;
+			this.btn1.UseVisualStyleBackColor = false;
 			this.btn1.Click += new System.EventHandler(this.btn1_Click);
 			// 
 			// btn2
 			// 
+			this.btn2.BackColor = System.Drawing.SystemColors.MenuHighlight;
 			this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btn2.Location = new System.Drawing.Point(84, 27);
+			this.btn2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btn2.Location = new System.Drawing.Point(126, 58);
+			this.btn2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.btn2.Name = "btn2";
-			this.btn2.Size = new System.Drawing.Size(66, 46);
+			this.btn2.Size = new System.Drawing.Size(99, 71);
 			this.btn2.TabIndex = 2;
 			this.btn2.Text = "2";
-			this.btn2.UseVisualStyleBackColor = true;
+			this.btn2.UseVisualStyleBackColor = false;
 			this.btn2.Click += new System.EventHandler(this.btn2_Click);
 			// 
 			// btn3
 			// 
+			this.btn3.BackColor = System.Drawing.SystemColors.MenuHighlight;
 			this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btn3.Location = new System.Drawing.Point(156, 27);
+			this.btn3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btn3.Location = new System.Drawing.Point(234, 58);
+			this.btn3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.btn3.Name = "btn3";
-			this.btn3.Size = new System.Drawing.Size(66, 46);
+			this.btn3.Size = new System.Drawing.Size(99, 71);
 			this.btn3.TabIndex = 3;
 			this.btn3.Text = "3";
-			this.btn3.UseVisualStyleBackColor = true;
+			this.btn3.UseVisualStyleBackColor = false;
 			this.btn3.Click += new System.EventHandler(this.btn3_Click);
 			// 
 			// btn4
 			// 
+			this.btn4.BackColor = System.Drawing.SystemColors.MenuHighlight;
 			this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btn4.Location = new System.Drawing.Point(228, 27);
+			this.btn4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btn4.Location = new System.Drawing.Point(341, 58);
+			this.btn4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.btn4.Name = "btn4";
-			this.btn4.Size = new System.Drawing.Size(66, 46);
+			this.btn4.Size = new System.Drawing.Size(99, 71);
 			this.btn4.TabIndex = 4;
 			this.btn4.Text = "4";
-			this.btn4.UseVisualStyleBackColor = true;
+			this.btn4.UseVisualStyleBackColor = false;
 			this.btn4.Click += new System.EventHandler(this.btn4_Click);
 			// 
 			// btn5
 			// 
+			this.btn5.BackColor = System.Drawing.SystemColors.MenuHighlight;
 			this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btn5.Location = new System.Drawing.Point(300, 27);
+			this.btn5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btn5.Location = new System.Drawing.Point(448, 58);
+			this.btn5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.btn5.Name = "btn5";
-			this.btn5.Size = new System.Drawing.Size(66, 46);
+			this.btn5.Size = new System.Drawing.Size(99, 71);
 			this.btn5.TabIndex = 5;
 			this.btn5.Text = "5";
-			this.btn5.UseVisualStyleBackColor = true;
+			this.btn5.UseVisualStyleBackColor = false;
 			this.btn5.Click += new System.EventHandler(this.btn5_Click);
 			// 
 			// btn6
 			// 
+			this.btn6.BackColor = System.Drawing.SystemColors.MenuHighlight;
 			this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btn6.Location = new System.Drawing.Point(372, 27);
+			this.btn6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btn6.Location = new System.Drawing.Point(555, 58);
+			this.btn6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.btn6.Name = "btn6";
-			this.btn6.Size = new System.Drawing.Size(66, 46);
+			this.btn6.Size = new System.Drawing.Size(99, 71);
 			this.btn6.TabIndex = 6;
 			this.btn6.Text = "6";
-			this.btn6.UseVisualStyleBackColor = true;
+			this.btn6.UseVisualStyleBackColor = false;
 			this.btn6.Click += new System.EventHandler(this.btn6_Click);
 			// 
 			// btn7
 			// 
+			this.btn7.BackColor = System.Drawing.SystemColors.MenuHighlight;
 			this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btn7.Location = new System.Drawing.Point(444, 27);
+			this.btn7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btn7.Location = new System.Drawing.Point(662, 58);
+			this.btn7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.btn7.Name = "btn7";
-			this.btn7.Size = new System.Drawing.Size(66, 46);
+			this.btn7.Size = new System.Drawing.Size(99, 71);
 			this.btn7.TabIndex = 7;
 			this.btn7.Text = "7";
-			this.btn7.UseVisualStyleBackColor = true;
+			this.btn7.UseVisualStyleBackColor = false;
 			this.btn7.Click += new System.EventHandler(this.btn7_Click);
 			// 
 			// menuStrip1
 			// 
+			this.menuStrip1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
 			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.helpToolStripMenuItem});
 			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(521, 24);
+			this.menuStrip1.Size = new System.Drawing.Size(778, 40);
 			this.menuStrip1.TabIndex = 8;
 			this.menuStrip1.Text = "menuStrip1";
 			// 
@@ -137,29 +162,31 @@ namespace Jackbox_Switcher
 			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.sendFeedbackToolStripMenuItem,
             this.aboutToolStripMenuItem});
+			this.helpToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlText;
 			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-			this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(81, 36);
 			this.helpToolStripMenuItem.Text = "Help";
 			// 
 			// sendFeedbackToolStripMenuItem
 			// 
 			this.sendFeedbackToolStripMenuItem.Name = "sendFeedbackToolStripMenuItem";
-			this.sendFeedbackToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.sendFeedbackToolStripMenuItem.Size = new System.Drawing.Size(282, 40);
 			this.sendFeedbackToolStripMenuItem.Text = "Send Feedback";
 			this.sendFeedbackToolStripMenuItem.Click += new System.EventHandler(this.sendFeedbackToolStripMenuItem_Click);
 			// 
 			// aboutToolStripMenuItem
 			// 
 			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(282, 40);
 			this.aboutToolStripMenuItem.Text = "About";
 			this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
 			// 
 			// Form1
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(521, 83);
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(778, 143);
 			this.Controls.Add(this.btn7);
 			this.Controls.Add(this.btn6);
 			this.Controls.Add(this.btn5);
@@ -171,8 +198,10 @@ namespace Jackbox_Switcher
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MainMenuStrip = this.menuStrip1;
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.Name = "Form1";
-			this.Text = "Jackbox Games Pack Switcher";
+			this.Opacity = 0.8D;
+			this.Text = "Party Pack Switcher";
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.menuStrip1.ResumeLayout(false);
 			this.menuStrip1.PerformLayout();
@@ -180,7 +209,6 @@ namespace Jackbox_Switcher
 			this.PerformLayout();
 
 		}
-
 		private void Form1_Load(object sender, EventArgs e)
 		{
 		}
